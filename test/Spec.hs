03319fe42c77de193648ce61323553acdd1711cc
Geoffrey Roberts <groberts@anchor.net.au>
2015-01-30 12:08:33 +1100

    Moving things around before we start a new example

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,16 +7,16 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Main where
 
-import Control.Monad
-import Data.Either (isRight)
-import Data.Proxy
+import           Control.Monad
+import           Data.Either         (isRight)
+import           Data.Proxy
 import qualified Language.PureScript as P
-import Test.Hspec
-import qualified Text.Parsec as TP
+import           Test.Hspec
+import qualified Text.Parsec         as TP
 
-import Servant.API
-import Servant.JQuery
-import Servant.PureScript
+import           Servant.API
+import           Servant.JQuery
+import           Servant.PureScript
 
 type TestAPI = "simple" :> ReqBody String :> Post Bool
           :<|> "has.extension" :> Get Bool
@@ -44,5 +44,5 @@ main = hspec .
             m `shouldContain` "get :: "
 
 shouldParse :: String -> Expectation
-shouldParse = 
+shouldParse =
     (`shouldSatisfy` isRight) . (TP.runParser P.parseModule (P.ParseState 0) "" <=< P.lex "")
