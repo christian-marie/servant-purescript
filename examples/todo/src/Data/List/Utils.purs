4c848297093dc35bbd2b592f2345ac8a4e43084e
Geoffrey Roberts <groberts@anchor.net.au>
2015-02-04 10:25:21 +1100

    Changed a few things in the todo example to use more methods from other libs

--- examples/todo/src/Data/List/Utils.purs
+++ examples/todo/src/Data/List/Utils.purs
@@ -9,18 +9,24 @@ module Data.List.Utils (
 ) where
 
 import qualified Data.Array as A
+import Data.Foldable
 import Data.List
 import Data.Tuple
 
 -- * Some list comparisons
 -- All nicked from MissingH
 
+-- | Given a delimiter and a list of items (or strings),
+-- join the items by using the delimiter.
 join :: forall a. [a] -> [[a]] -> [a]
-join delim l = A.concat (intersperse delim l)
+join = intercalate
 
+-- | Given a list and a replacement list, replaces each occurance
+-- of the search list with the replacement list in the operation list.
 replace :: forall a. (Eq a) => [a] -> [a] -> [a] -> [a]
 replace old new l = join new <<< split old $ l
 
+-- | Given a delimiter and a list (or string), split into components.
 split :: forall a. (Eq a) => [a] -> [a] -> [[a]]
 split _ [] = []
 split delim str = first : second
@@ -44,21 +50,23 @@ spanList func list@(x:xs) =
        else Tuple [] list
     where spanned = spanList func xs
 
+-- | Tests whether a list begins with another list.
 startswith :: forall a. (Eq a) => [a] -> [a] -> Boolean
 startswith = isPrefixOf
 
+-- | Tests whether a list ends with another list.
 endswith :: forall a. (Eq a) => [a] -> [a] -> Boolean
 endswith = isSuffixOf
 
 -- * Ported from Data.List
 
-isPrefixOf              :: forall a. (Eq a) => [a] -> [a] -> Boolean
-isPrefixOf [] _         =  true
-isPrefixOf _  []        =  false
-isPrefixOf (x:xs) (y:ys)=  x == y && isPrefixOf xs ys
+isPrefixOf               :: forall a. (Eq a) => [a] -> [a] -> Boolean
+isPrefixOf [] _          = true
+isPrefixOf _  []         = false
+isPrefixOf (x:xs) (y:ys) = x == y && isPrefixOf xs ys
 
 isSuffixOf              :: forall a. (Eq a) => [a] -> [a] -> Boolean
-isSuffixOf x y          =  A.reverse x `isPrefixOf` A.reverse y
+isSuffixOf x y          = A.reverse x `isPrefixOf` A.reverse y
 
 intersperse             :: forall a. a -> [a] -> [a]
 intersperse _   []      = []
